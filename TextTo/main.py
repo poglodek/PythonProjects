8619256b223b16fb5d6be7a91b870c8b653adda7
poglodek <pawelogll@gmail.com>
2021-10-24 18:07:31 +0200

    adding words spliting

--- TextTo/main.py
+++ TextTo/main.py
@@ -1,11 +1,24 @@
 import gtts
+import os
+import requests
 from playsound import playsound
+from bs4 import BeautifulSoup
 
 text_file_name = "text.txt"
 audio_file_name = "Audio.mp3"
+text_file =""
+
+
 
 with open(text_file_name) as text:
-    tts = gtts.gTTS(text.read(), lang="pl")
-    tts.save(audio_file_name)
+    text_file = text.read()
 
-playsound(audio_file_name)
+tts = gtts.gTTS(text_file, lang="pl")
+tts.save(audio_file_name)
+
+words = text_file.replace("\n"," ").split(" ")
+
+print(words)
+
+
+os.remove(audio_file_name)

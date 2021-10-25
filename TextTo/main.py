b1dcda63d8f1b601afa4d58f3821e4a19eccb4e2
poglodek <pawelogll@gmail.com>
2021-10-25 08:33:12 +0200

    distinct_words

--- TextTo/main.py
+++ TextTo/main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 text_file_name = "text.txt"
 audio_file_name = "Audio.mp3"
 text_file =""
-
+distinct_words = [] 
 
 
 with open(text_file_name) as text:
@@ -17,8 +17,12 @@ tts = gtts.gTTS(text_file, lang="pl")
 tts.save(audio_file_name)
 
 words = text_file.replace("\n"," ").split(" ")
-
 print(words)
 
+for word in words:
+    if word  not in distinct_words:
+        distinct_words.append(word)
+print(distinct_words)
+#wyrazy zescrapowac z zdj z google grafika a potem zrobić film
 
 os.remove(audio_file_name)
